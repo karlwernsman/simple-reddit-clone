392b900576f42df649838670e4c23b4c9fc90005
karl wernsman <wernsmank@gmail.com>
2022-10-05 13:02:30 -0700

    Added wireframe and SUPABASE link and api key

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,6 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://hnwrqbuafnmeqngyhxku.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhud3JxYnVhZm5tZXFuZ3loeGt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ5OTYwNjYsImV4cCI6MTk4MDU3MjA2Nn0.y8u3fp6hVDf8D1THV6tqYwhbuLfea8LVEIUUrcF4qhY';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /* Auth related functions */
